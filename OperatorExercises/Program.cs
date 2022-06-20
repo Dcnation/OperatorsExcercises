fe3ee185b6d409b13e650ab5575fcf984edb9f3a
Devin Carlisle <devincarlisle@Devins-Air.attlocal.net>
2022-06-20 10:31:24 -0500

    adding

---
+++ OperatorExercises/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace OperatorExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //For division, create two integer variables called a and b.
+
+            int a = 17;
+            int b = 4;
+            var quotient = a / b;
+            var remainder = a % b;
+
+            Console.WriteLine(a + "/" + b + " is " + quotient + " remainder " + remainder);
+
+
+            // define a method - which performs some fuctionality. than can
+            // be used over and over again
+
+            var radius = double.Parse(Console.ReadLine());
+
+            var area = Math.PI * Math.Pow(radius, 2);
+
+             Console.WriteLine($"The area of a circle with a radius of (radius) is (area).");
+
+            Console.WriteLine();
+
+            Console.WriteLine("What is the radius of your circle?");
+
+            var radius = double.Parse(Console.ReadLine());
+
+            var area = Math.PI * Math.Pow(radius, 2);
+
+            
+
+            Console.WriteLine($"The area of a circle with a radius of {radius} is {area}.");
+        }
+    }
+}
+
